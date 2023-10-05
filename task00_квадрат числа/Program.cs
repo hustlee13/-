28f13b5e97e81a17b7f2324f4024277ef7b9412b
Egor <diffor@bk.ru>
2023-10-05 11:38:34 +0300

    Решил задачи первого семинара

---
+++ task00_квадрат числа/Program.cs
@@ -0,0 +1,8 @@
+﻿/*0. Напишите программу, которая на вход принимает число и выдаёт его квадрат (число умноженное на само себя). */
+
+System.Console.WriteLine("Введите число: ");
+int userNumder = Convert.ToInt32(Console.ReadLine());
+int square = userNumder * userNumder;
+System.Console.WriteLine(square);
+
+
---
+++ task01/Program.cs
@@ -0,0 +1,13 @@
+﻿System.Console.WriteLine("Введите число: ");
+int userNumber1 = Convert.ToInt32(Console.ReadLine());
+System.Console.WriteLine("Введите число 2: ");
+int userNumber2 = Convert.ToInt32(Console.ReadLine());
+
+if (userNumber1 == userNumber2 * userNumber2)
+{
+    System.Console.WriteLine("Да");
+}
+else
+{
+    System.Console.WriteLine("Нет");
+}
